e578668e6922f9079f9bb45620236ec6e567e7d6
Stevens <korwinstevens@quickenloans.com>
2018-05-22 16:31:17 -0400

    Working global tags

--- src/Teletemp/main.go
+++ src/Teletemp/main.go
@@ -1,45 +1,40 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
-	"strings"
 )
 
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm() //Parse url parameters passed, then parse the response packet for the POST body (request body)
-	// attention: If you do not call ParseForm method, the following data can not be obtained form
-	fmt.Println(r.Form) // print information on server side.
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
-	fmt.Fprintf(w, "Hello astaxie! %v", r.URL) // write data to response
-}
-
-func login(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method) //get request method
+// Serve the Form
+func envForm(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		t, _ := template.ParseFiles("index.html")
 		t.Execute(w, nil)
-	} else {
-		r.ParseForm()
-		// logic part of log in
-		fmt.Println("username:", r.Form["username"])
-		fmt.Println("password:", r.Form["password"])
-		fmt.Fprintf(w, "Hello %v!", r.Form["username"]) // write data to response
+	}
+
+}
+
+// Get the input from the form and genrate the tags
+func telegraf(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		t, _ := template.ParseFiles("telegraf.html")
+
+		globalTags := []string{
+			"Environment = " + r.PostFormValue("env"),
+			"Role = " + r.PostFormValue("role"),
+			"Application = " + r.PostFormValue("app"),
+		}
+
+		t.Execute(w, globalTags)
+
 	}
 }
 
 func main() {
-	http.HandleFunc("/", sayhelloName) // setting router rule
-	http.HandleFunc("/login", login)
-	err := http.ListenAndServe(":9090", nil) // setting listening port
+	http.HandleFunc("/", envForm) // setting router rule
+	http.HandleFunc("/telegraf", telegraf)
+	err := http.ListenAndServe(":8080", nil) // setting listening port
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
