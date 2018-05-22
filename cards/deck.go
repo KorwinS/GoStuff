7c5a37791c308b1df622d0073fe65b2c2aa4cca8
Stevens <korwinstevens@quickenloans.com>
2018-05-22 07:33:59 -0400

    cleanup

--- cards/deck.go
+++ cards/deck.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"time"
-	"os"
-	"io/ioutil"
 	"fmt"
-	"strings"
+	"io/ioutil"
 	"math/rand"
+	"os"
+	"strings"
+	"time"
 )
 
 // Create a new type of 'deck'
@@ -15,7 +15,7 @@ type deck []string
 
 func newDeck() deck {
 	cards := deck{}
-	cardSuits := []string {"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
 	for _, suit := range cardSuits {
@@ -26,7 +26,7 @@ func newDeck() deck {
 	return cards
 }
 
-func (d deck) print()  {
+func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
